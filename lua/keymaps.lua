5daf152f63d1b13042e369528e9ba877c52738f2
Precious <preciousnyaupane3@gmail.com>
2025-10-27 21:56:59 -0700

    fixed bug with :wq not closing nvim with one buffer

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,11 +8,24 @@ end, { desc = "Focus NvimTree" })
 
 -- Remap :wq to write and close the current buffer, ignoring Neo-tree
 vim.api.nvim_create_user_command("Wq", function()
-  if vim.bo.filetype ~= "neo-tree" then
-    vim.cmd("write")     -- save file
-    vim.cmd("bdelete")   -- close only current buffer
+  -- Get a table of all *listed* buffers
+  local listed_buffers = vim.fn.getbufinfo({buflisted = 1})
+  local num_listed_buffers = #listed_buffers
+
+  if num_listed_buffers > 1 then
+    -- MORE than 1 buffer open: Save and close current buffer
+    if vim.bo.filetype ~= "neo-tree" then
+      vim.cmd("write") -- Save if it's a normal file
+    end
+    vim.cmd("bdelete") -- Close just this buffer
+  else
+    -- ONE buffer open: Save and quit Neovim
+    if vim.bo.filetype ~= "neo-tree" then
+      vim.cmd("write") -- Save if it's a normal file
+    end
+    vim.cmd("quit") -- Quit nvim
   end
 end, {})
 
+-- This maps :wq to run your :Wq command
 vim.cmd("cnoreabbrev wq Wq")
-
