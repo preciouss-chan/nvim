0662020c5a5b44b79c4241390cd79874ab570e75
Precious <preciousnyaupane3@gmail.com>
2025-10-28 20:58:39 -0700

    fixed python file treesitter warning

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,9 +1,62 @@
-require("nvim-treesitter.configs").setup({
-  ensure_installed = {
-    "lua", "python", "javascript", "typescript", "tsx",
-    "html", "css", "c", "cpp", "kotlin"
-  },
-  highlight = { enable = true },
-  indent = { enable = true },
-})
+-- This file should be at: lua/plugins/treesitter.lua
 
+return {
+  "nvim-treesitter/nvim-treesitter",
+
+  -- This `build` step is a good practice to automatically run :TSUpdate
+  -- This will install/update parsers on plugin updates.
+  build = ":TSUpdate",
+
+  -- This is a very common companion plugin, you can add it if you want
+  -- text-objects. You'll see it in the config below.
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+
+  config = function()
+    -- We must call setup *inside* the config function.
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      -- A list of parser names, or "all".
+      -- Add any languages you use here.
+      ensure_installed = {
+        "python",
+        "lua",
+        "vim",
+        "vimdoc",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "bash",
+        "markdown",
+        "json",
+        "yaml",
+        "c",
+        "cpp",
+        -- Add more languages here!
+      },
+
+      -- Install parsers synchronously (only relevant for :TSInstall)
+      sync_install = false,
+
+      -- Automatically install missing parsers when entering a buffer
+      -- Highly recommended!
+      auto_install = true,
+
+      -- === Core Modules ===
+
+      highlight = {
+        enable = true,
+        -- You can disable highlighting for specific languages if they cause issues
+        -- disable = { "c", "rust" },
+      },
+
+      indent = {
+        enable = true,
+      },
+
+    })
+  end,
+}
